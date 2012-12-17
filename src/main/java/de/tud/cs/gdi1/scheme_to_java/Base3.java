8d471ed99e051e38c0d733a12253c5fc50cbef5a
Michael Eichberg <mail@michael-eichberg.de>
2012-12-17 17:39:22 +0100

    first template

---
+++ src/main/java/de/tud/cs/gdi1/scheme_to_java/Base3.java
@@ -0,0 +1,14 @@
+package de.tud.cs.gdi1.scheme_to_java;
+
+public class Base3 {
+
+    
+    static String toBase3(int value) {
+       return ""; 
+    }
+    
+    public static void main(String[] args) {
+       System.out.println(toBase3(Integer.parseInt(args[0])));
+    }
+
+}
