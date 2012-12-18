0a1b09062e82dd3273c7336083e96da5d974056f
Michael Eichberg <mail@michael-eichberg.de>
2012-12-18 10:16:50 +0100

    added another method to explain the "if" expression

--- src/main/java/de/tud/cs/gdi1/scheme_to_java/Base3.java
+++ src/main/java/de/tud/cs/gdi1/scheme_to_java/Base3.java
@@ -6,12 +6,17 @@ public class Base3 {
 
     static String toBase3Recursive(int value) {
         if (value >= 3) {
-            return toBase3(value / 3) + String.valueOf(value % 3);
+            return toBase3Recursive(value / 3) + String.valueOf(value % 3);
         } else {
             return String.valueOf(value % 3);
         }
     }
 
+    static String toBase3Recursive2(int value) {
+        return (value >= 3 ? toBase3Recursive2(value / 3) : "") + String.valueOf(value % 3);
+    }
+
+    @SuppressWarnings("all")
     static String toBase3(int value) {
         String s = "";
         do {
@@ -26,14 +31,14 @@ public class Base3 {
         out.println(toBase3(1));
         out.println(toBase3(2));
         out.println(toBase3(3));
-        out.println(toBase3(10));
-        out.println(toBase3(11));
-        out.println(toBase3(12));
-        out.println(toBase3(13));
-        out.println(toBase3(14));
-        out.println(toBase3(15));
-        out.println(toBase3(16));
-        out.println(toBase3(17));
+        out.println(toBase3Recursive(10));
+        out.println(toBase3Recursive(11));
+        out.println(toBase3Recursive(12));
+        out.println(toBase3Recursive(13));
+        out.println(toBase3Recursive2(14));
+        out.println(toBase3Recursive2(15));
+        out.println(toBase3Recursive2(16));
+        out.println(toBase3Recursive2(17));
         // out.println(toBase3(Integer.parseInt(args[0])));
     }
 
