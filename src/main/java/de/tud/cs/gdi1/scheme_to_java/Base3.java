1a8566a225fb8645c73e71be387e4caf6de92223
Michael Eichberg <mail@michael-eichberg.de>
2012-12-18 07:54:49 +0100

    we now have a small method to convert a dezimal number to a ternary number

--- src/main/java/de/tud/cs/gdi1/scheme_to_java/Base3.java
+++ src/main/java/de/tud/cs/gdi1/scheme_to_java/Base3.java
@@ -1,14 +1,40 @@
 package de.tud.cs.gdi1.scheme_to_java;
 
+import static java.lang.System.out;
+
 public class Base3 {
 
-    
-    static String toBase3(int value) {
-       return ""; 
+    static String toBase3Recursive(int value) {
+        if (value >= 3) {
+            return toBase3(value / 3) + String.valueOf(value % 3);
+        } else {
+            return String.valueOf(value % 3);
+        }
     }
-    
+
+    static String toBase3(int value) {
+        String s = "";
+        do {
+            s = value % 3 + s;
+            value = value / 3;
+        } while (value > 0);
+        return s;
+    }
+
     public static void main(String[] args) {
-       System.out.println(toBase3(Integer.parseInt(args[0])));
+        out.println(toBase3(0));
+        out.println(toBase3(1));
+        out.println(toBase3(2));
+        out.println(toBase3(3));
+        out.println(toBase3(10));
+        out.println(toBase3(11));
+        out.println(toBase3(12));
+        out.println(toBase3(13));
+        out.println(toBase3(14));
+        out.println(toBase3(15));
+        out.println(toBase3(16));
+        out.println(toBase3(17));
+        // out.println(toBase3(Integer.parseInt(args[0])));
     }
 
 }
