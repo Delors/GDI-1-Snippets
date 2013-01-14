bbbc7ca87fea322d26f6eaf83f6bdf039988ff51
Michael Eichberg <mail@michael-eichberg.de>
2013-01-14 14:50:03 +0100

    used blocks to make it possible uncomment all code

--- src/main/java/de/tud/cs/gdi1/scheme_to_java/CompareStrings.java
+++ src/main/java/de/tud/cs/gdi1/scheme_to_java/CompareStrings.java
@@ -6,14 +6,23 @@ import java.io.InputStreamReader;
 public class CompareStrings {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("First string: ");
-        String s1 = in.readLine();
-        System.out.print("Second string: ");
-        String s2 = in.readLine();
+        {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("First string: ");
+            String s1 = in.readLine();
+            System.out.print("Second string: ");
+            String s2 = in.readLine();
+            System.out.println("==        ? " + (s2 == s1));
+            System.out.println(".equals() ? " + (s2.equals(s1)));
+        }
 
-        System.out.println("==        ? " + (s2 == s1));
-        System.out.println(".equals() ? " + (s2.equals(s1)));
+        {
+            String s1 = "jj";
+            String s2 = "jj";
+
+            System.out.println("==        ? " + (s2 == s1));
+            System.out.println(".equals() ? " + (s2.equals(s1)));
+        }
     }
 
 }
